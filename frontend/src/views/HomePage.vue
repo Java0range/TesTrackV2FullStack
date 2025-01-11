18bde011f1a956c75e583af2ffe3cd2c3c324cef
Java0range <javared234@gmail.com>
2025-01-11 19:57:12 +0300

    Fix: Main Title

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -16,7 +16,7 @@ const openPanel = () => {
     <div class="bg-white shadow-2xl flex p-5 rounded-3xl mt-40 gap-3 max-sm:flex-col w-1/2 max-sm:w-full md:w-3/4 xl:w-1/2">
       <img src="/favicon.ico" alt="logo" class="h-full w-full max-sm:h-12 max-sm:w-12 md:w-1/2" />
       <div>
-        <h3 class="text-xl"><b>TesTrackV2 Beta</b> - это OpenSource Web приложение для проведения пробников ЕГЭ по информатике, нацеленое в первую очередь на повышение новыков для сдачи экзамена</h3>
+        <h3 class="text-xl"><b>TesTrackV2 Beta</b> - это OpenSource Web приложение для проведения пробников ЕГЭ по информатике, нацеленное в первую очередь на повышение навыков для сдачи экзамена</h3>
         <button @click="openPanel" class="bg-green-500 hover:bg-green-400 text-white font-bold py-3 px-4 rounded-full transition-all mt-3 text-base">
           Панель учителя
         </button>
